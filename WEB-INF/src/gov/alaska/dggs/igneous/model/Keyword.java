fd47b2c3c18230b62a12ac96ac8d05b0c330e32d
Christopher Ramey <christopher.ramey@alaska.gov>
2013-12-19 18:52:07 +0000

    Updated keyword, added keyword group

--- WEB-INF/src/gov/alaska/dggs/igneous/model/Keyword.java
+++ WEB-INF/src/gov/alaska/dggs/igneous/model/Keyword.java
@@ -8,10 +8,14 @@ public class Keyword implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	private int id;
-	private String name, description, code;
+	private String name, alias, description;
+
+	private KeywordGroup group;
 
 	public int getID(){ return id; }
 	public String getName(){ return name; }
+	public String getAlias(){ return alias; }
 	public String getDescription(){ return description; }
-	public String getCode(){ return code; }
+
+	public KeywordGroup getGroup(){ return group; }
 }
---
+++ WEB-INF/src/gov/alaska/dggs/igneous/model/KeywordGroup.java
@@ -0,0 +1,15 @@
+package gov.alaska.dggs.igneous.model;
+
+import java.io.Serializable;
+
+
+public class KeywordGroup implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String name;
+
+	public int getID(){ return id; }
+	public String getName(){ return name; }
+}
