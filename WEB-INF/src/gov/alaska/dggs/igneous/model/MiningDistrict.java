91472ca13c44d98af44836f951fdc7ecb4dbb413
Christopher Ramey <christopher.ramey@alaska.gov>
2014-01-03 01:59:11 +0000

    Fixed bug from removal of region field in mining_district

--- WEB-INF/src/gov/alaska/dggs/igneous/model/MiningDistrict.java
+++ WEB-INF/src/gov/alaska/dggs/igneous/model/MiningDistrict.java
@@ -8,11 +8,10 @@ public class MiningDistrict implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	private int id;
-	private String name, region;
+	private String name;
 	private String wkt;
 
 	public int getID(){ return id; }
 	public String getName(){ return name; }
-	public String getRegion(){ return region; }
 	public String getWKT(){ return wkt; }
 }
