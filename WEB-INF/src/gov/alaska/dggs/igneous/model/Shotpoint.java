2ce564fa2c1bb0075fe6def2c0e710ecc02713d1
Christopher Ramey <christopher.ramey@alaska.gov>
2014-02-24 22:51:56 +0000

    Fixed change to shotpoint number

--- WEB-INF/src/gov/alaska/dggs/igneous/model/Shotpoint.java
+++ WEB-INF/src/gov/alaska/dggs/igneous/model/Shotpoint.java
@@ -2,6 +2,7 @@ package gov.alaska.dggs.igneous.model;
 
 import java.io.Serializable;
 import java.util.List;
+import java.math.BigDecimal;
 
 
 public class Shotpoint implements Serializable
@@ -9,10 +10,10 @@ public class Shotpoint implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	private int id;
-	private String name;
+	private BigDecimal number;
 	private Shotline shotline;
 
 	public int getID(){ return id; }
-	public String getName(){ return name; }
+	public BigDecimal getNumber(){ return number; }
 	public Shotline getShotline(){ return shotline; }
 }
