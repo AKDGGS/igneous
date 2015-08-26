4d9846a15b219cd0ceae81ac6645e76fb37d678d
Christopher Ramey <christopher.ramey@alaska.gov>
2015-08-26 15:30:48 +0000

    Finished basic inventory import tool

--- WEB-INF/src/gov/alaska/dggs/igneous/page/InventoryImportServlet.java
+++ WEB-INF/src/gov/alaska/dggs/igneous/page/InventoryImportServlet.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -24,9 +26,22 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.FileCleanerCleanup;
 
+import org.supercsv.prefs.CsvPreference;
+import org.supercsv.io.CsvMapReader;
+
+import flexjson.JSONSerializer;
+import gov.alaska.dggs.igneous.transformer.ExcludeTransformer;
+
 
 public class InventoryImportServlet extends HttpServlet
 {
+	private static JSONSerializer serializer;
+	static {
+		serializer = new JSONSerializer();
+		serializer.transform(new ExcludeTransformer(), void.class);
+	}
+
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		doGet(request, response);
@@ -49,14 +64,44 @@ public class InventoryImportServlet extends HttpServlet
 				ServletFileUpload upload = new ServletFileUpload(factory);
 				List<FileItem> items = upload.parseRequest(request);
 
-				for(FileItem item : items){
-					// Ignore stuff that isn't a file
-					if(item.isFormField()) continue;
+				SqlSession sess = IgneousFactory.openSession();
+				int count = 0;
+				try {
+					for(FileItem item : items){
+						// Ignore stuff that isn't a file
+						if(item.isFormField()) continue;
 
-					results.append("File: " + item.getName());
+						InputStreamReader isr = new InputStreamReader(item.getInputStream());
+						try {
+							CsvMapReader cmr = new CsvMapReader(
+								isr, CsvPreference.EXCEL_PREFERENCE
+							);
+
+							String[] header = cmr.getHeader(true);
+							for(Map<String,String> row; (row = cmr.read(header)) != null;){
+								sess.insert(
+									"gov.alaska.dggs.igneous.Inventory.insertStash",
+									serializer.serialize(row)
+								);
+								count++;
+							}
+						} finally {
+							isr.close();
+						}
+					}
+					sess.commit();
+
+					results.append("Success, inserted " + count + " rows.");
+				} catch(Exception ex){
+					// Rollback on error, trickle the error up
+					sess.rollback();
+					throw ex;
 				}
 			}
 		} catch(Exception ex){
+			ex.printStackTrace();
+
+			// Return the error to the user
 			results.append("Error: " + ex.getMessage());
 		}
 
