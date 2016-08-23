b077025bd220b527ad0593f95588a09341b97423
Christopher Ramey <christopher.ramey@alaska.gov>
2016-08-23 20:19:38 +0000

    Added monochrome OSM, set as default

--- js/search.js
+++ js/search.js
@@ -332,52 +332,56 @@ function init()
 	});
 
 	var baselayers = {
+		'Open Street Maps Monochrome': L.tileLayer(
+			'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
+			{ minZoom: 3, maxZoom: 18, zIndex: 1 }
+		),
 		'Open Street Maps': new L.TileLayer(
 			'//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-			{ minZoom: 3, maxZoom: 19, zIndex: 1 }
+			{ minZoom: 3, maxZoom: 19, zIndex: 2 }
 		),
 		'OpenTopoMap': new L.TileLayer(
 			'http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-			{ minZoom: 3, maxZoom: 17, zIndex: 2  }
+			{ minZoom: 3, maxZoom: 17, zIndex: 3  }
 		),
 		'ESRI Imagery': new L.TileLayer(
 			'//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-			{ minZoom: 3, maxZoom: 19, zIndex: 3 }
+			{ minZoom: 3, maxZoom: 19, zIndex: 4 }
 		),
 		'ESRI Topographic': new L.TileLayer(
 			'//server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-			{ minZoom: 3, maxZoom: 19, zIndex: 4 }
+			{ minZoom: 3, maxZoom: 19, zIndex: 5 }
 		),
 		'ESRI Shaded Relief': new L.TileLayer(
 			'//server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}',
-			{ minZoom: 3, maxZoom: 13, zIndex: 5 }
+			{ minZoom: 3, maxZoom: 13, zIndex: 6 }
 		),
 		'ESRI DeLorme': new L.TileLayer(
 			'//server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}',
-			{ minZoom: 3, maxZoom: 11, zIndex: 6 }
+			{ minZoom: 3, maxZoom: 11, zIndex: 7 }
 		),
 		'ESRI National Geographic': new L.TileLayer(
 			'//server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}',
-			{ minZoom: 3, maxZoom: 16, zIndex: 7 }
+			{ minZoom: 3, maxZoom: 16, zIndex: 8 }
 		),
 		'GINA Satellite': new L.TileLayer(
 			'http://tiles.gina.alaska.edu/tilesrv/bdl/tile/{x}/{y}/{z}',
-			{ minZoom: 3, mazZoom: 15, zIndex: 8 }
+			{ minZoom: 3, mazZoom: 15, zIndex: 9 }
 		),
 		'GINA Topographic': new L.TileLayer(
 			'http://tiles.gina.alaska.edu/tilesrv/drg/tile/{x}/{y}/{z}',
-			{ minZoom: 3, maxZoom: 12, zIndex: 9 }
+			{ minZoom: 3, maxZoom: 12, zIndex: 10 }
 		),
 		'Stamen Watercolor': new L.TileLayer(
 			'http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',
-			{ minZoom: 3, maxZoom: 16, subdomains: 'abcd', zIndex: 10 }
+			{ minZoom: 3, maxZoom: 16, subdomains: 'abcd', zIndex: 11 }
 		)
 	};
 
 	var overlays = {
 		'Quadrangles': new L.TileLayer(
 			'http://tiles.gina.alaska.edu/tilesrv/quad_google/tile/{x}/{y}/{z}',
-			{ minZoom: 3, maxZoom: 16, zIndex: 11 }
+			{ minZoom: 3, maxZoom: 16, zIndex: 12 }
 		)
 	};
 
@@ -385,7 +389,7 @@ function init()
 		attributionControl: false,
 		zoomControl: false,
 		worldCopyJump: true,
-		layers: [ baselayers['Open Street Maps'] ]
+		layers: [ baselayers['Open Street Maps Monochrome'] ]
 	});
 
 	map.addLayer(features);
